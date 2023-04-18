d4ad63261b850850c836fb7a77ca1fd22a9587cc
世强 王 <2236046598@qq.com>
2023-04-18 17:53:14 +0800

    1.2 统计字符串中每个字母出现的次数，以Map结构返回

--- src/main/java/com/ksyun/whgc/wangshiqiang/firstQuestion/StringUtils.java
+++ src/main/java/com/ksyun/whgc/wangshiqiang/firstQuestion/StringUtils.java
@@ -7,5 +7,14 @@ public class StringUtils {
     public static String reverse(String str) {
         return new StringBuilder(str).reverse().toString();
     }
+    // 1.2 统计字符串中每个字母出现的次数，以Map结构返回
+    public static Map<Character, Integer> countOccurrences(String str) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        return map;
+    }
 
 }
